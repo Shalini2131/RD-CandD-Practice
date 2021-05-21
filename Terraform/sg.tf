f47181a11b6c2a30a302d0acee6914f1cca7157d
Shalini Kallepalli <Shalini_Kallepalli@epam.com>
2021-05-21 11:48:29 +0530

    added cloudfront output images

--- Terraform/sg.tf
+++ Terraform/sg.tf
@@ -3,13 +3,16 @@ resource "aws_security_group" "terraform_ssh_sg" {
   description = "Allowing SSH from anywhere"
   vpc_id      = "${aws_vpc.terraform-vpc.id}"
   name        = "terraform_ec2_ssh_sg"
-
+  
   ingress {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
     from_port   = 22
     to_port     = 22
   }
+
+  
+
   tags = {
     Name = "ec2-SSH-sg"
   }
